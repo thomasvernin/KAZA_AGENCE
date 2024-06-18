1a68b84b9ae08ed1eef4c30bba88cba193e3af69
Thomas Vernin <tvernin49100@gmail.com>
2024-06-18 16:31:02 +0200

    Dernière modifications

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,14 +1,26 @@
 import useAccordion from '@/hooks/useAccordion'
 import PropTypes from 'prop-types'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import './Accordion.scss'
 
 export default function Accordion({ title = '', content = '' }) {
     const detailsRef = useRef()
     const summaryRef = useRef()
     const contentRef = useRef()
+    const [isOpen, setIsOpen] = useState(false)
 
     useAccordion(detailsRef, summaryRef, contentRef)
+
+    const toggleCollapse = () => {
+        const content = contentRef.current;
+        setIsOpen(!isOpen); 
+
+        if (isOpen) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
 
     const processedContent = Array.isArray(content) ? (
         <ul className="list">
@@ -21,9 +33,10 @@ export default function Accordion({ title = '', content = '' }) {
     )
 
     return (
-        <details className="accordion" ref={detailsRef}>
-            <summary className="accordion__title" ref={summaryRef}>
+        <details className={`accordion ${isOpen ? 'open' : ''}`} ref={detailsRef}>
+            <summary className="accordion__title" ref={summaryRef} onClick={toggleCollapse}>
                 {title}
+                <span className={`accordion__icon ${isOpen ? 'open' : ''}`}></span> {/* Ajout de l'icône ici */}
             </summary>
             <div className="accordion__content" ref={contentRef}>
                 {processedContent}
@@ -34,8 +47,17 @@ export default function Accordion({ title = '', content = '' }) {
 
 Accordion.propTypes = {
     title: PropTypes.string,
-    content: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+    content: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string)
+    ]),
 }
+
+
+
+
+
+
 
 
 
